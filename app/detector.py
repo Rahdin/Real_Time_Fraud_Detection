eec59179e71feefc2bc81b01610eb81428d694d0
Rahdin Zaman <rahdinzaman@gmail.com>
2025-12-08 23:19:04 -0500

    Added the detector file. Added transcation velocity checker function

---
+++ app/detector.py
@@ -0,0 +1,32 @@
+import joblib
+import redis
+import os
+import numpy as np
+from datetime import datetime
+
+class FraudDetector:
+    def __init__(self):
+        # Load the ML Model
+        try:
+            self.model = joblib.load("app/isolation_forest.joblib")
+        except FileNotFoundError:
+            self.model = None # Handle gracefully if model isn't trained yet
+
+        # Connect to Redis (Cache)
+        redis_host = os.getenv("REDIS_HOST", "localhost")
+        self.redis = redis.Redis(host=redis_host, port=6379, db=0, decode_responses=True)
+
+    def check_velocity(self, user_id: str) -> bool:
+        """
+        Rule: Flag if user makes > 3 transactions in 60 seconds.
+        """
+        key = f"tx_count:{user_id}"
+        
+        # Atomically increment transaction count
+        current_count = self.redis.incr(key)
+        
+        # If it's the first transaction, set expiry for 60 seconds
+        if current_count == 1:
+            self.redis.expire(key, 60)
+            
+        return current_count > 3
