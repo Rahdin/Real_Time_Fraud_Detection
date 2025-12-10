aca03f5693942c9b9810f4743ae77d731fc453a7
Rahdin Zaman <rahdinzaman@gmail.com>
2025-12-09 19:52:41 -0500

    Fixed training data format. Model working as expected

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,27 +1,34 @@
-"""
-This file containts the script to train a dummy Isolation Forest model
-on synthetic banking transaction data for fraud detection.
-
-"""
-
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import IsolationForest
 import joblib
-import logging
 
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 def train_dummy_model():
-    logger.info("Generating synthetic banking data...")
+    n_samples = 1000
     
-  
-    X_normal = 50 + 10 * np.random.randn(1000, 3) 
+    
+    f1_amount = np.random.normal(50, 15, n_samples)
+    
+    
+    f2_risk = np.random.randint(0, 20, n_samples)
     
    
-    X_outliers = 5000 + 1000 * np.random.randn(50, 3)
+    f3_hour = np.random.randint(0, 24, n_samples)
+    
+    
+    X_normal = np.column_stack((f1_amount, f2_risk, f3_hour))
+    
+    
+    n_anomalies = 50
+    
+    
+    a1_amount = np.random.normal(2000, 500, n_anomalies)
+    a2_risk = np.random.randint(50, 100, n_anomalies)
+    a3_hour = np.random.randint(0, 24, n_anomalies)
+    
+    X_outliers = np.column_stack((a1_amount, a2_risk, a3_hour))
+    
     
     X = np.vstack([X_normal, X_outliers])
     
@@ -29,10 +36,7 @@ def train_dummy_model():
     clf = IsolationForest(random_state=42, contamination=0.05)
     clf.fit(X)
     
+    joblib.dump(clf, "app/isolation_forest.joblib")
    
-    model_path = "app/isolation_forest.joblib"
-    joblib.dump(clf, model_path)
-    logger.info(f"Model trained and saved to {model_path}")
-
 if __name__ == "__main__":
     train_dummy_model()
